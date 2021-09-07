3f852afd898593e447bdcbd0bcfd677f6d70a9ec
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-09-07 23:28:26 +0300

    add search section to views

--- app/views.py
+++ app/views.py
@@ -3,6 +3,8 @@ from app import app
 from .request import get_movies
 from .request import get_movies,get_movie
 from .request import get_movies,get_movie,search_movie
+from flask import render_template,request,redirect,url_for
+
 
 
 # Views
@@ -17,8 +19,15 @@ def index():
     popular_movies = get_movies('popular')
     upcoming_movie = get_movies('upcoming')
     now_showing_movie = get_movies('now_playing')
+
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
+
+    search_movie = request.args.get('movie_query')
+
+    if search_movie:
+        return redirect(url_for('search',movie_name=search_movie))
+    else:
+        return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
 
 
