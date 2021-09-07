a68a3a2a1e1d82aea50db24c014e9a503c180c4b
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-09-07 22:35:15 +0300

    created ccs customfile

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app import app
 from .request import get_movies
+from .request import get_movies,get_movie
 
 # Views
 @app.route('/')
@@ -19,10 +20,13 @@ def index():
 
 
 
-@app.route('/movie/<int:movie_id>')
-def movie(movie_id):
+@app.route('/movie/<int:id>')
+def movie(id):
 
     '''
     View movie page function that returns the movie details page and its data
     '''
-    return render_template('movie.html', id = movie_id)    
+    movie = get_movie(id)
+    title = f'{movie.title}'
+
+    return render_template('movie.html',title = title,movie = movie)
