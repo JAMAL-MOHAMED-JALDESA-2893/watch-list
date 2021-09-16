8cfc407d5a66240f46b4a55a395d62d2b93ba726
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-09-16 18:39:34 +0300

    add updates

--- config.py
+++ config.py
@@ -6,7 +6,15 @@ class Config:
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:sicario2793@localhost/watchlist'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+#  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
 class ProdConfig(Config):
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 export MOVIE_API_KEY='002061da868dcd44f424596b0305421c'
 export SECRET_KEY='Your secret key'
-
+export MAIL_USERNAME='jayhamo2793@gmail.com'
+export MAIL_PASSWORD='JAMALHASSANMOHAMED2793'
 python3 manager.py server
